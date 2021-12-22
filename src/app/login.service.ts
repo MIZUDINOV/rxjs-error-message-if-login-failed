c323dfa02283a2b33e10ccdd7f3dfe08f3b9be11
Mukhamed <izudinov.work@gmail.com>
2021-12-22 17:41:01 +0300

    add login service observable

---
+++ src/app/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { delay, map, Observable, of, tap } from 'rxjs';
+
+@Injectable()
+export class LoginService extends Observable<string> {
+  constructor() {
+    super((subscriber) =>
+      of(Math.random())
+        .pipe(
+          tap(() => console.log('Выполняется вход...')),
+          delay(1000),
+          map((response) => {
+            if (response > 0.5) {
+              throw new Error('Ошибка при входе!');
+            }
+            return 'Muhammad Izudinov';
+          })
+        )
+        .subscribe(subscriber)
+    );
+  }
+}
